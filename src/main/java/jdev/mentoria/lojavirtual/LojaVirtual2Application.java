36f7ffe76d64339ea3b7391c112d340cf328ead3
Halisson Nascimento <halissonp@hotmail.com>
2024-09-26 21:44:15 -0300

    commit inicial projeto loja virtual java

---
+++ src/main/java/jdev/mentoria/lojavirtual/LojaVirtual2Application.java
@@ -0,0 +1,13 @@
+package jdev.mentoria.lojavirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaVirtual2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaVirtual2Application.class, args);
+	}
+
+}
