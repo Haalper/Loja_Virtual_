74b523e1f10cd055805a0f0c5f95e976942fd926
Halisson Nascimento <halissonp@hotmail.com>
2024-10-22 01:04:17 -0300

    Atualizando o projeto com as últimas alterações

---
+++ src/main/java/jdev/mentoria/lojavirtual/model/NotaFiscalCompra.java
@@ -0,0 +1,54 @@
+package jdev.mentoria.lojavirtual.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+
+
+import javax.persistence.ConstraintMode;
+import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+
+@Entity
+@Table(name = "nota_fiscal_compra")
+@SequenceGenerator(name = "seq_nota_fiscal_compra", sequenceName = "seq_nota_fiscal_compra", allocationSize = 1, initialValue = 1)
+public class NotaFiscalCompra implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_nota_fiscal_compra")
+	private Long id;
+	
+	
+	private String numeroNota;
+	private String serieNota;
+	private String descricaoObs;
+	private BigDecimal valorTotal;
+	private BigDecimal valorDesconto;
+	private BigDecimal valorIcms;
+	
+	@Temporal(TemporalType.DATE)
+	private Date dataCompra;
+	
+	@ManyToOne(targetEntity = Pessoa.class)
+	@JoinColumn(name = "pessoa_id", nullable = false, 
+	foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fk"))
+	private Pessoa pessoa;
+	//a nossa loja virtual está fazendo compra de produtos (de uma pessoa - Física ou Jurídica)
+	
+	@ManyToOne
+	@JoinColumn(name = "conta_pagar_id", nullable = false, 
+	foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "conta_pagar_fk"))
+	private ContaPagar contaPagar;
+	//esta compra está gerando uma conta a pagar - Muitas notas fiscais de compra para uma Conta a Pagar
+
+}
